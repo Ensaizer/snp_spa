d159b2f6b378b8cdafc2748a5310d16865842724
zhenyaka89 <zhenyaka89@gmail.com>
2024-01-22 12:12:37 +0300

    auth slice

--- client/src/components/ui/RegistrationForm.tsx
+++ client/src/components/ui/RegistrationForm.tsx
@@ -1,8 +1,8 @@
-import { Box, Button, TextField, Typography } from '@mui/material';
-import React from 'react'
+import { Box, Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField, Typography } from '@mui/material';
+import React from 'react';
 
 export default function RegistrationForm() {
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <Typography variant="h3" gutterBottom>
@@ -10,7 +10,7 @@ export default function RegistrationForm() {
       </Typography>
       <Box component="form" onSubmit={(e) => void dispatch(registrationHandlerThunk(e))}>
         <Box mb={2}>
-          <TextField name="name" label="ФИО/Название организации" type="text" required />
+          <TextField name="name" label="ФИО" type="text" required />
         </Box>
         <Box mb={2}>
           <TextField name="email" label="Почта" type="email" required />
@@ -24,10 +24,53 @@ export default function RegistrationForm() {
         <Box mb={2}>
           <TextField name="deliveryAddress" label="Адрес доставки" type="text" required />
         </Box>
+        <FormControl>
+          <FormLabel id="userType">Тип</FormLabel>
+          <RadioGroup
+            aria-labelledby="userType-radio-buttons-group"
+            name="userType"
+            // value={value}
+            // onChange={handleChange}
+          >
+            <FormControlLabel value="физическое лицо" control={<Radio />} label="Физическое лицо" />
+            <FormControlLabel value="юридическое лицо" control={<Radio />} label="Юридическое лицо" />
+          </RadioGroup>
+        </FormControl>
+        <FormControl>
+          <FormLabel id="roleId">Тип</FormLabel>
+          <RadioGroup
+            aria-labelledby="userType-radio-buttons-group"
+            name="roleId"
+            // value={value}
+            // onChange={handleChange}
+          >
+            <FormControlLabel value="1" control={<Radio />} label="Покупатель" />
+            <FormControlLabel value="2" control={<Radio />} label="Продавец" />
+          </RadioGroup>
+        </FormControl>
+        <Box mb={2}>
+          <TextField name="orgName" label="Полное наименование организации" type="text" required />
+        </Box>
+        <Box mb={2}>
+          <TextField name="INN" label="ИНН" type="text" required />
+          <TextField name="KPP" label="КПП" type="text" required />
+        </Box>
+        <Box mb={2}>
+          <TextField name="ORGN" label="ОГРН" type="text" required />
+        </Box>
+        <Box mb={2}>
+          <TextField name="legalAddress" label="Юридический адрес" type="text" required />
+        </Box>
+        <Box mb={2}>
+          <TextField name="currAccount" label="Расчетный счёт" type="text" required />
+        </Box>
+        <Box mb={2}>
+          <TextField name="corrAccount" label="Корреспондентский счёт" type="text" required />
+        </Box>
         <Button type="submit" variant="contained" fullWidth>
           Зарегистрироваться
         </Button>
       </Box>
     </Box>
-  )
+  );
 }
---
+++ client/src/services/authService.ts
@@ -0,0 +1,67 @@
+import axios from 'axios';
+import type { AuthState, LoginFormData, RegistrationFormData, UserType } from '../types/auth/index';
+
+export const authInstance = axios.create({
+  baseURL: 'http://localhost:3000/api/v1/',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+class AuthService {
+  static async refresh(): Promise<AuthState> {
+    const response = await authInstance.get<{ user: UserType; accessToken: string }>(
+      '/tokens/refresh',
+    );
+
+    const authState: AuthState = {
+      ...response.data,
+      user: { ...response.data.user, status: 'authenticated' },
+    };
+    return authState;
+  }
+
+  static async login(formData: LoginFormData): Promise<AuthState> {
+    const response = await authInstance.post<{ user: UserType; accessToken: string }>(
+      '/auth/login',
+      formData,
+    );
+    const authState: AuthState = {
+      ...response.data,
+      user: { ...response.data.user, status: 'authenticated' },
+    };
+    return authState;
+  }
+
+  static async registration(formData: RegistrationFormData): Promise<AuthState> {
+    const response = await authInstance.post<{ user: UserType; accessToken: string }>(
+      '/auth/registration',
+      formData,
+    );
+    const authState: AuthState = {
+      ...response.data,
+      user: { ...response.data.user, status: 'authenticated' },
+    };
+    return authState;
+  }
+
+  static async logout(): Promise<void> {
+    try {
+      await authInstance.get('/auth/logout');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async check(): Promise<AuthState> {
+    const response = await authInstance.get<{ user: UserType; accessToken: string }>('/auth/check');
+    const authState: AuthState = {
+      ...response.data,
+      user: { ...response.data.user, status: 'authenticated' },
+    };
+    return authState;
+  }
+}
+
+export default AuthService;
