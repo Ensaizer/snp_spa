7df0c15eadb8e11c00c81231a36587a9421635cd
Антон Рокунец <rokunecanton@gmail.com>
2024-01-22 23:54:49 +0300

    added slider categories

--- client/src/components/pages/MainPage.tsx
+++ client/src/components/pages/MainPage.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useGetAllCategoriesQuery} from "../../store/categorySlise/category.ts";
 
-function MainPage() {
-  return <div />;
+function MainPage():JSX.Element {
+   const {data, isLoading, error} = useGetAllCategoriesQuery('');
+   const [state, setSatate] = useState(1)
+  return (
+      <>
+        {isLoading && <h2>Loading...</h2>}
+        {error && <h2>Sorry we have some error: {error.message}</h2>}
+        { data && (
+            <section className="categories">
+              <ul className="categories__list">
+                { data.map(el => <li className="categories__item"
+                                     key={el.id}
+                                     onClick={()=>setSatate(el.id)}
+                >{el.name}</li>) }
+              </ul>
+                {
+                    data.map(el => <img
+                        src={`${el.id}.jpg`}
+                        alt="trucks"
+                        className={el.id === state ? 'categories__img active' : 'categories__img '}/>)
+                }
+
+
+            </section>
+
+        )}
+      </>
+
+  );
 }
 
 export default MainPage;
---
+++ client/src/store/categorySlise/category.ts
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import type {CategoryType} from "../../types";
+
+// Define a service using a base URL and expected endpoints
+export const categoryApi = createApi({
+    reducerPath: 'pokemonApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/categories' }),
+    endpoints: (builder) => ({
+        getAllCategories: builder.query<CategoryType[], string>({
+            query: () => `/`,
+        }),
+    }),
+})
+
+
+export const {  useGetAllCategoriesQuery  } = categoryApi
