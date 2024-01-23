c7503a9d8bc1dead8de0e2cc55700973e0c42d64
Ensaizer <ensaizer.x@gmail.com>
2024-01-23 18:03:33 +0300

    fixes

--- client/src/store/userSlice/userSlice.ts
+++ client/src/store/userSlice/userSlice.ts
@@ -1,49 +1,50 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import type {UserType} from "../../types/auth";
+import type { UserType } from '../../types/auth';
 
 // Define a service using a base URL and expected endpoints
 export const usersApi = createApi({
-    reducerPath: 'usersApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/users' }),
-    tagTypes: ['Users'],
-    endpoints: (builder) => ({
-        getAllUsers: builder.query<UserType[], string>({
-            query: () => `/`,
-            providesTags: (result) =>
-                result ? result.map(({ id }) => ({ type: 'Users', id } as const)) : [],
-        }),
-        getAllCustomers: builder.query<UserType[], string>({
-            query: () => `/customers`,
-        }),
-        getAllPartners: builder.query<UserType[], string>({
-            query: () => `/partners`,
-        }),
-        deleteUser: builder.mutation<{ id: number }, { id: number }>({
-            query(id) {
-                return {
-                    url: `/${id.toString()}`,
-                    method: 'DELETE',
-                }
-            },
-            // invalidatesTags: (result, id) => [{ type: 'Users', id }],
-            invalidatesTags: ['Users']
-        }),
-        updateUser: builder.mutation<UserType, UserType>({
-            query(data) {
-                return {
-                    url: `/${data.id.toString()}`,
-                    method: 'PATCH',
-                    data,
-                }
-            },
-            invalidatesTags: ['Users'],
-        }),
-    })
+  reducerPath: 'usersApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/users' }),
+  tagTypes: ['Users'],
+  endpoints: (builder) => ({
+    getAllUsers: builder.query<UserType[], string>({
+      query: () => `/`,
+      providesTags: (result) =>
+        result ? result.map(({ id }) => ({ type: 'Users', id }) as const) : [],
+    }),
+    getAllCustomers: builder.query<UserType[], string>({
+      query: () => `/customers`,
+    }),
+    getAllPartners: builder.query<UserType[], string>({
+      query: () => `/partners`,
+    }),
+    deleteUser: builder.mutation<{ id: number }, { id: number }>({
+      query({ id }) {
+        return {
+          url: `/${id.toString()}`,
+          method: 'DELETE',
+        };
+      },
+      // invalidatesTags: (result, id) => [{ type: 'Users', id }],
+      invalidatesTags: ['Users'],
+    }),
+    updateUser: builder.mutation<UserType, UserType>({
+      query(data) {
+        return {
+          url: `/${data.id.toString()}`,
+          method: 'PATCH',
+          data,
+        };
+      },
+      invalidatesTags: ['Users'],
+    }),
+  }),
 });
 
-export const { useGetAllUsersQuery,
-    useDeleteUserMutation,
-    useGetAllCustomersQuery,
-    useGetAllPartnersQuery,
-    useUpdateUserMutation
+export const {
+  useGetAllUsersQuery,
+  useDeleteUserMutation,
+  useGetAllCustomersQuery,
+  useGetAllPartnersQuery,
+  useUpdateUserMutation,
 } = usersApi;
