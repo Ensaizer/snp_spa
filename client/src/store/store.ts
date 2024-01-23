8c85c7f51c9c1a7e02623a96ce84cc26bf09537a
Anton-CSS <9251580262@mail.ru>
2024-01-23 12:06:02 +0300

    add userRouter

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -2,15 +2,17 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/auth/authReducer';
 import productSlice from './slice/products';
 import {categoryApi} from "./categorySlise/category.ts";
+import {usersApi} from "./userSlice/userSlice.ts";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     productsState: productSlice,
     [categoryApi.reducerPath]: categoryApi.reducer,
+    [usersApi.reducerPath]: usersApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(categoryApi.middleware),
+      getDefaultMiddleware().concat(categoryApi.middleware, usersApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- server/db/seeders/20240122094803-products.js
+++ server/db/seeders/20240122094803-products.js
@@ -1,6 +1,89 @@
 /** @type {import('sequelize-cli').Migration} */
+const bcrypt = require('bcrypt');
+
 module.exports = {
   async up(queryInterface) {
+      const roles = [
+          {
+              name: 'customer',
+          },
+          {
+              name: 'partner',
+          },
+          {
+              name: 'admin',
+          },
+      ];
+      await queryInterface.bulkInsert('Roles', roles);
+      
+      const getUsers = async() =>{
+          const users = [
+              {
+                  name: 'Антон',
+                  email: 'rokunecanton@gmail.com',
+                  password: await bcrypt.hash('1234', 10),
+                  phone: '+7-926-707-10-09',
+                  userType: 'физическое лицо',
+                  deliveryAddress: 'Московская область Красногорский район д. Путилково ул. Сходненская д.38 кв 147',
+                  discount: 0,
+                  roleId: 1,
+                  isApproved: true
+              },
+              {
+                  name: 'Владимир ',
+                  email: 'vladimir@gmail.com',
+                  password: await bcrypt.hash('1234', 10),
+                  phone: '+7-917-906-56-79',
+                  userType: 'физическое лицо',
+                  deliveryAddress: 'Московская область Красногорский район д. Путилково ул. Сходненская д.38 кв 147',
+                  discount: 0,
+                  roleId: 3,
+                  isApproved: true
+              },
+              {
+                  name: 'Евгения',
+                  email: 'vgev@gmail.com',
+                  password: await bcrypt.hash('1234', 10),
+                  phone: '+7-926-674-89-75',
+                  userType: 'Юридическое лицо',
+                  deliveryAddress: 'Москва ул. Строителей д.46 кв 56',
+                  discount: 0,
+                  roleId: 2,
+                  isApproved: true,
+              },
+              {
+                  name: 'Анатолий',
+                  email: 'toly@gmail.com',
+                  password: await bcrypt.hash('1234', 10),
+                  phone: '+7-926-674-89-75',
+                  userType: 'Юридическое лицо',
+                  deliveryAddress: 'Москва ул. Строителей д.46 кв 56',
+                  discount: 0,
+                  roleId: 1,
+                  isApproved: true,
+              }];
+          return users
+      }
+      await queryInterface.bulkInsert('Users', await getUsers());
+      
+      const brands = [
+          {
+              name:'Volvo Construction',
+              description: 'одна из производственных компаний, входящих в концерн Volvo. Осуществляет разработку, производство и обслуживание строительно-дорожных машин под маркой Volvo, оборудования для строительной и смежных отраслей.',
+              logoPath: './VC.jpg'
+          },
+          {
+              name:'Volvo Penta',
+              description: 'Volvo Penta — шведская компания, входящая в Volvo Group, производитель судовых и промышленных двигателей.',
+              logoPath: './penta.jpg'
+          },
+          {
+              name:'Volvo Trucks',
+              description: 'шведская автомобилестроительная компания, один из мировых лидеров по производству тяжёлых грузовиков. В переводе с латыни «volvo» означает «я кручусь» или «я качусь». Принадлежит второму в мире по величине производителю грузовиков «Volvo Group».',
+              logoPath: './trucks.jpg'
+          },
+      ];
+      await queryInterface.bulkInsert('Brands', brands);
     const categories = [
       {
         name: "Кузовные запчасти",
