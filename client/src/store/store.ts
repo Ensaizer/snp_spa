74a4aa79ed347149edef22e4c7ab705d566d3daa
Ensaizer <ensaizer.x@gmail.com>
2024-01-23 12:08:15 +0300

    inProgress

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/auth/authReducer';
 import productSlice from './slice/products';
+import cartReducer from './slices/cartSlice/cartReducer';
 import { categoryApi } from './categorySlise/category.ts';
 
 export const store = configureStore({
@@ -8,8 +9,8 @@ export const store = configureStore({
     auth: authReducer,
     productsState: productSlice,
     [categoryApi.reducerPath]: categoryApi.reducer,
+    cart: cartReducer,
   },
-
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(categoryApi.middleware),
 });
 
