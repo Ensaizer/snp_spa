366f2292eed89b7bf39e8ab92c3d954da9542d2c
Anton-CSS <9251580262@mail.ru>
2024-01-25 09:55:45 +0300

    order

--- server/src/routes/orderRouter.js
+++ server/src/routes/orderRouter.js
@@ -5,13 +5,36 @@ const orderRouter = express.Router();
 
 orderRouter.post("/", async (req, res) => {
     const body = req.body;
-    const order = await Order.create(body)
-    if(order){
-        return res.status(200).json(order)
-    }
-    // const result = Cart.findAll({ where:{userId: order.userId} })
-    // console.log(result)
-    // const entry = Entry.cre
+    let order = await Order.create(body);
+    order = order.get();
+    
+    let result = await Cart.findAll({
+        where:{userId: order.userId},
+        include:{
+            model: Product,
+            attributes: ['price']
+        }
+    });
+    
+    result = result.map(item => item.get());
+    const destroyIds = result.map(item => item.id);
+    let entries = result.map(item => {
+        return {
+            ...item,
+            price: item.Product.price
+        }
+    });
+    
+    entries = entries.map((item) =>{
+        delete item.Product;
+        delete item.createdAt;
+        delete item.updatedAt;
+        item.orderId = order.id;
+        return item
+    });
+    await Entry.bulkCreate(entries, {returning: true});
+    await Cart.destroy({where: {id: destroyIds}});
+    res.sendStatus(200)
 });
 
 module.exports = orderRouter;
