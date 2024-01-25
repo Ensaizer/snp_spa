a796767bd2ddcd5faa1245447ba744cbed034794
Ensaizer <ensaizer.x@gmail.com>
2024-01-25 16:10:56 +0300

    fixes

--- client/src/components/pages/MainPage.tsx
+++ client/src/components/pages/MainPage.tsx
@@ -15,30 +15,46 @@ function MainPage(): JSX.Element {
         <Carousel />
         {/* </Container> */}
 
-      {isLoading && <h2>Loading...</h2>}
-      {error && <h2>Sorry we have some error: {error.message}</h2>}
-      {data && (
-        // <section className="categories">
-        <Paper elevation={16} sx={{display: 'flex', backgroundСolor: '#F0EFFA', overflow: 'hidden'}}>
-          <ul className="categories__list">
+        {isLoading && <h2>Loading...</h2>}
+        {error && <h2>Sorry we have some error: {error.message}</h2>}
+        {data && (
+          // <section className="categories">
+          <Paper
+            elevation={16}
+            sx={{ display: 'flex', backgroundСolor: '#F0EFFA', overflow: 'hidden' }}
+          >
+            <ul className="categories__list">
+              {data.map((el) => (
+                <li className="categories__item" key={el.id} onClick={() => setSatate(el.id)}>
+                  {el.name}
+                </li>
+              ))}
+            </ul>
             {data.map((el) => (
-              <li className="categories__item" key={el.id} onClick={() => setSatate(el.id)}>
-                {el.name}
-              </li>
+              <img
+                key={el.id}
+                src={`${el.id}.jpg`}
+                alt="trucks"
+                className={el.id === state ? 'categories__img active' : 'categories__img '}
+              />
             ))}
-          </ul>
-          {data.map((el) => (
-            <img
-              key={el.id}
-              src={`${el.id}.jpg`}
-              alt="trucks"
-              className={el.id === state ? 'categories__img active' : 'categories__img '}
-            />
-          ))}
           </Paper>
-        // </section>
-      )}
-            </Container>
+        )}
+        <img
+          src="./main_2.png"
+          alt="main"
+          className="main__img"
+          style={{ objectFit: 'cover', width: '1155px', paddingTop: '50px' }}
+        />
+        <img
+          src="./main_1.png"
+          alt="main"
+          className="main__img"
+          style={{ objectFit: 'cover', width: '1155px', paddingTop: '50px' }}
+        />
+      </Container>
+
+      <Container />
     </>
   );
 }
